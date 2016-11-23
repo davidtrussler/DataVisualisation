d7a2311b1c486f57bfe74f020ad3356c29d6c078
David Trussler <mail@davidtrussler.net>
2016-11-23 16:30:23 +0000

    Comment: Binds data to dynaically generated DOM elements

--- js/app.js
+++ js/app.js
@@ -3,5 +3,21 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   console.log('init!')
 
-  d3.select('body').append('p').text('something');
+  bindData();
+}
+
+
+function bindData() {
+  console.log('bindData!');
+
+  var dataset = [5, 10, 15, 20, 25];
+
+  d3.select('body')
+    .selectAll('p')
+    .data(dataset)
+    .enter()
+    .append('p')
+    .text(function(d) {
+      return d;
+    });
 }
