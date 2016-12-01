b0a02413e536cc67048e17d98022688ff8d2c53c
David Trussler <mail@davidtrussler.net>
2016-12-01 16:39:37 +0000

    Comment: Draws SVG shapes

--- js/app.js
+++ js/app.js
@@ -3,22 +3,34 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   console.log('init!')
 
-  drawData();
+  drawSVG();
 }
 
 
-function drawData() {
-  console.log('drawData!');
+function drawSVG() {
+  console.log('drawSVG!');
 
   var dataset = [5, 10, 15, 20, 25];
 
-  d3.select('body')
-    .selectAll('div')
-    .data(dataset)
-    .enter()
-    .append('div')
-    .attr('class', 'bar')
-    .style('height', function(d) {
-      return d + 'px';
+  var w = 500,
+      h = 54;
+
+  var svg = d3.select('body')
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h);
+
+  var circles = svg.selectAll('circle')
+                .data(dataset)
+                .enter()
+                .append('circle');
+
+  circles
+    .attr('cx', function(d, i) {
+    return (i * 50) + 25
+  })
+    .attr('cy', h/2)
+    .attr('r', function(d) {
+      return d;
     });
 }
